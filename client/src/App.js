cfdbe07d21c6b7c53f4ef7e7ffb116f8e4a744de
rocketArtillery-Katusha <tikt2664@gmail.com>
2023-07-03 10:27:46 +0300

    checking user for auth

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,43 @@
+import Header from './сomponents/Header/Header';
+import PostsPage from './pages/PostsPage/PostsPage';
+import AuthPage from './pages/AuthPage/AuthPage';
+import { useDispatch, useSelector } from 'react-redux';
+import { getMe } from './redux/features/authSlice';
+import { useEffect } from 'react';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
+  const dispatch = useDispatch();
+  const isAuth = useSelector((state) => state.auth.user);
 
+  useEffect(() => {
+    dispatch(getMe());
+  }, [dispatch]);
+
+
+  return (
+    <div className='wrapper'>
+      {isAuth === null ? '' : typeof isAuth === 'object' ? (
+        <div className='wrapper-container'>
+          <header className='header'>
+            <Header />
+          </header>
+          <div className='container'>
+            <Routes>
+              <Route path='/' element={<PostsPage />} />
+            </Routes>
+          </div>
+        </div>
+      ) : (
+        <div className='wrapper-container'>
+          <div className='container'>
+            <AuthPage />
+          </div>
+        </div>
+      )}
     </div>
   );
-}
+};
 
 export default App;
