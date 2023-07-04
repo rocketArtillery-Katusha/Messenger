4a54bd047ad7146af389ce9e520215fa7aa2c80f
rocketArtillery-Katusha <tikt2664@gmail.com>
2023-07-04 11:26:40 +0300

    add profile slice

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authSlice from './features/authSlice';
 import postSlice from './features/postSlice';
+import profileSlice from './features/profileSlice';
 
 
 export const store = configureStore({
     reducer: {
         auth: authSlice,
-        post: postSlice
+        post: postSlice,
+        profile: profileSlice
     },
 });
