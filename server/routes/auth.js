89fee90b00a756fb7530bb0917b82d7cc2a89618
rocketArtillery-Katusha <tikt2664@gmail.com>
2023-07-13 14:16:15 +0300

    add new route

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { register, login, getMe } from '../controllers/auth.js';
+import { register, login, getMe, updateUserinfo } from '../controllers/auth.js';
 import { loginValidation, registerValidation } from '../middleware/authValidatuon.js';
 import { checkAuth } from '../middleware/checkAuth.js'
 
@@ -7,6 +7,7 @@ const router = Router();
 
 router.post('/register', registerValidation, register);
 router.post('/login', loginValidation, login);
+router.put('/update-user-info', checkAuth, updateUserinfo);
 router.get('/get-me', checkAuth, getMe);
 
 export default router;
--- server/routes/post.js
+++ server/routes/post.js
@@ -1,11 +1,12 @@
 import { Router } from 'express';
 import { checkAuth } from '../middleware/checkAuth.js';
-import { createPost, getAllPosts, createComment, getComments, likePost } from '../controllers/post.js';
+import { createPost, getAllPosts, getMyPosts, createComment, getComments, likePost } from '../controllers/post.js';
 
 const router = Router();
 
 router.post('/create-post', checkAuth, createPost);
 router.get('/get-all-posts', checkAuth, getAllPosts);
+router.get('/get-my-posts', checkAuth, getMyPosts);
 router.post('/create-comment', checkAuth, createComment);
 router.get('/get-comments:id', checkAuth, getComments);
 router.patch('/like:id', checkAuth, likePost);
