f2be4d1ca50c68c4ac3bb327e4d614eb08380289
rocketArtillery-Katusha <tikt2664@gmail.com>
2023-08-21 12:21:08 +0300

    add new reducer

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,12 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authSlice from './features/authSlice';
 import postSlice from './features/postSlice';
-import actionUserSlice from './features/actionUserSlice';
+import socketSlice from './features/socketSlice';
 
 export const store = configureStore({
     reducer: {
         auth: authSlice,
         post: postSlice,
-        actionsUsers: actionUserSlice,
+        socket: socketSlice,
     },
 });
