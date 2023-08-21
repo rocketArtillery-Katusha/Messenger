473ddbf42d4ed9e062969b4a0ca6fadf4eb90528
rocketArtillery-Katusha <tikt2664@gmail.com>
2023-08-21 12:06:24 +0300

    add new routes and socket func

--- client/src/App.js
+++ client/src/App.js
@@ -2,45 +2,75 @@ import Header from './сomponents/Header/Header';
 import PostsPage from './pages/PostsPage/PostsPage';
 import AuthPage from './pages/AuthPage/AuthPage';
 import ProfilePage from './pages/ProfilePage/ProfilePage';
-import FrendsPage from './pages/FrendsPage/FrendsPage';
+import FriendsPage from './pages/FriendsPage/FriendsPage';
+import ConversationPage from './pages/ConversationPage/ConversationPage';
+import MessagesPage from './pages/MessagesPage/MessagesPage';
 import { useDispatch, useSelector } from 'react-redux';
-import { getMe } from './redux/features/authSlice';
-import { useEffect } from 'react';
+import { getMe, socketUpdateMe } from './redux/features/authSlice';
+import { useEffect, useRef } from 'react';
 import { Route, Routes } from 'react-router-dom';
+import { socketContext } from './utils/socketContext';
+import io from 'socket.io-client';
 import './App.css';
 
 function App() {
   const dispatch = useDispatch();
-  const isAuth = useSelector((state) => state.auth.user);
+  const isAuth = useSelector((state) => state.auth.me);
+  const socket = useRef();
 
   useEffect(() => {
     dispatch(getMe());
   }, [dispatch]);
 
+  useEffect(() => {
+    if (isAuth) {
+      socket.current = io("ws://localhost:8900");
+      socket.current.emit("addUser", isAuth._id);
+      socket.current.on("deletedFriend", ({ updatedUser, deletedFriend }) => {
+        dispatch(socketUpdateMe({ updatedUser, deletedFriend }));
+      });
+      socket.current.on("requestsFriends", ({ updatedUser, newRequest }) => {
+        dispatch(socketUpdateMe({ updatedUser, newRequest }));
+      });
+      socket.current.on("cancledRequest", ({ updatedUser, deletedRequest }) => {
+        dispatch(socketUpdateMe({ updatedUser, deletedRequest }));
+      });
+      socket.current.on("addedFriend", ({ updatedUser, addedFriend }) => {
+        dispatch(socketUpdateMe({ updatedUser, addedFriend }));
+      });
+      socket.current.on("getMessage", (data) => {
+        // dispatch(updateMessageState(data))
+      });
+    }
+  }, [dispatch, isAuth]);
+
   return (
-    <div className='wrapper'>
-      {isAuth === null ? '' : typeof isAuth === 'object' ? (
-        <div className='wrapper-container'>
-          <header className='header'>
-            <Header />
-          </header>
-          <div className='container'>
-            <Routes>
-              <Route path='/' element={<PostsPage />} />
-              <Route path='frends' element={<FrendsPage />} />
-              <Route path='frends/profile/:id' element={<ProfilePage />} />
-              <Route path='profile' element={<ProfilePage />} />
-            </Routes>
+    <socketContext.Provider value={socket}>
+      <div className='wrapper'>
+        {isAuth === null ? '' : typeof isAuth === 'object' ? (
+          <div className='wrapper-container'>
+            <header className='header'>
+              <Header />
+            </header>
+            <div className='container'>
+              <Routes>
+                <Route path='/' element={<PostsPage />} />
+                <Route path='friends' element={<FriendsPage />} />
+                {['profile', 'profile/:id'].map((path, index) => <Route path={path} element={<ProfilePage />} key={index} />)}
+                <Route path='messages' element={<MessagesPage />} />
+                <Route path='messages/:id' element={<ConversationPage />} />
+              </Routes>
+            </div>
           </div>
-        </div>
-      ) : (
-        <div className='wrapper-container'>
-          <div className='container'>
-            <AuthPage />
+        ) : (
+          <div className='wrapper-container'>
+            <div className='container'>
+              <AuthPage />
+            </div>
           </div>
-        </div>
-      )}
-    </div>
+        )}
+      </div>
+    </socketContext.Provider>
   );
 };
 
