7317dc90acb280db0b7861f6aa0d85e7879a5ccd
rocketArtillery-Katusha <tikt2664@gmail.com>
2023-08-22 11:01:22 +0300

    add new routes

--- server/routes/post.js
+++ server/routes/post.js
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 import { checkAuth } from '../middleware/checkAuth.js';
-import { createPost, getAllPosts, getMyPosts, createComment, getComments, likePost } from '../controllers/post.js';
+import { createPost, getAllPosts, getMyPosts, getPostsById, createComment, getComments, likePost } from '../controllers/post.js';
 
 const router = Router();
 
@@ -9,6 +9,7 @@ router.get('/get-all-posts', checkAuth, getAllPosts);
 router.get('/get-my-posts', checkAuth, getMyPosts);
 router.post('/create-comment', checkAuth, createComment);
 router.get('/get-comments:id', checkAuth, getComments);
+router.get('/get-posts-by-id:id', getPostsById);
 router.patch('/like:id', checkAuth, likePost);
 
 export default router;
