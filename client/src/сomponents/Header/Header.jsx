09ebec6c70b49c6672cc277d22d4c7a503a5a4fa
rocketArtillery-Katusha <tikt2664@gmail.com>
2023-08-22 10:32:39 +0300

    add counter

--- client/src/сomponents/Header/Header.jsx
+++ client/src/сomponents/Header/Header.jsx
@@ -1,15 +1,20 @@
 import React from 'react';
-import { Link } from 'react-router-dom'
+import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 import './header.css';
 
 const Header = () => {
+    const me = useSelector((state) => state.auth.me);
+
     return (
         <div className='header__inner'>
             <Link to='/' className='logo'>Messenger</Link>
             <ul className="header__menu">
-                <li><Link to='messages'>Сообщения</Link></li>
-                <li><Link to='frends'>Друзья</Link></li>
-                <li><a href='0'>Фотографии</a></li>
+                <li><Link to='/messages'>Сообщения</Link></li>
+                <li>
+                    <Link to='/friends'>Друзья</Link>
+                    {me.friendRequests.length > 0 && (<div className='counter'>{me.friendRequests.length}</div>)}
+                </li>
                 <li><Link to='/profile'>Профиль</Link></li>
             </ul>
         </div>
